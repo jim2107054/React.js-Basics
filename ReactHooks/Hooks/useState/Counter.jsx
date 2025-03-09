bf8f17f43dbc2ebdf38d57b20576c7e8583916db
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-09 13:05:23 +0600

    useState for variable creation

--- ReactHooks/Hooks/useState/Counter.jsx
+++ ReactHooks/Hooks/useState/Counter.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+  // let counter = 0;
+
+  const [counter, setcounter] = useState(0);
+  
+  const increment = () => {
+    setcounter(counter + 1);
+  }
+  const decrement = () => {
+    if(counter > 0){
+      setcounter(counter - 1);
+    }
+    else{
+      alert('Counter cannot be negative');
+    }
+    
+  }
   return (
-    <div>Counter</div>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
   )
 }
 
